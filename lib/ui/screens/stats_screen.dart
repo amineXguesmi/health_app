b501d1e771498d259f9d94ebc4c7e19769717e8a
Hassan <mohamedhassanamara7@gmail.com>
2023-11-11 13:14:00 +0100

    bottom bottom_navigation

---
+++ lib/ui/screens/stats_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Stats Screen'),
+      ),
+    );
+  }
+}
