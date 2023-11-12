cf38360e1a4a7273fdbe325adeb58221b855fe9c
amine <mohamedamineguesmi@insat.u-carthage.tn>
2023-11-12 14:29:50 +0100

    add home_screen

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:health_app/ui/widgets/bottom_navagation_widget.dart';
 import 'package:health_app/ui/screens/device_screen.dart';
 import 'package:health_app/ui/screens/health_screen.dart';
 import 'package:health_app/ui/screens/home_screen.dart';
 import 'package:health_app/ui/screens/schedule_screen.dart';
 import 'package:health_app/ui/screens/settings_screen.dart';
 import 'package:health_app/ui/screens/stats_screen.dart';
+import 'package:health_app/ui/widgets/bottom_navagation_widget.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -20,7 +20,7 @@ class _MainScreenState extends State<MainScreen> {
 
   /// List of pages to be shown in PageView
   final List<Widget> bottomBarPages = <Widget>[
-     HomeScreen(),
+    const HomeScreen(),
     const DeviceScreen(),
     const ScheduleScreen(),
     const SettingsScreen(),
