463ad7c2e37cd34a9e3c52d427da884e66032930
amine <mohamedamineguesmi@insat.u-carthage.tn>
2023-11-13 14:01:11 +0100

    merge stats_screen

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -3,7 +3,6 @@ import 'package:health_app/ui/screens/device_screen.dart';
 import 'package:health_app/ui/screens/health_screen.dart';
 import 'package:health_app/ui/screens/home_screen.dart';
 import 'package:health_app/ui/screens/schedule_screen.dart';
-import 'package:health_app/ui/screens/settings_screen.dart';
 import 'package:health_app/ui/screens/stats_screen.dart';
 import 'package:health_app/ui/widgets/bottom_navagation_widget.dart';
 
@@ -23,9 +22,8 @@ class _MainScreenState extends State<MainScreen> {
     const HomeScreen(),
     const DeviceScreen(),
     ScheduleScreen(),
-    const SettingsScreen(),
-    StatsScreen(),
     const HealthScreen(),
+    StatsScreen(),
   ];
   @override
   Widget build(BuildContext context) {
